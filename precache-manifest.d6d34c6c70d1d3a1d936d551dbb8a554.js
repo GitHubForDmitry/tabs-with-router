af8ce4d7a5132a4ebdf338524402474623fac787
GitHubForDmitry <dmitrykoka@gmail.com>
2019-08-02 11:36:10 +0300

    Updates

---
+++ precache-manifest.d6d34c6c70d1d3a1d936d551dbb8a554.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcd112041e2f0e4c3e0537d735a588f",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "a62883a84d1af96b31ce",
+    "url": "/tabs-with-router/static/css/main.716beffd.chunk.css"
+  },
+  {
+    "revision": "2fb1a2b4c8031c741475",
+    "url": "/tabs-with-router/static/js/2.fe7fbde2.chunk.js"
+  },
+  {
+    "revision": "a62883a84d1af96b31ce",
+    "url": "/tabs-with-router/static/js/main.88e6e4d4.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
